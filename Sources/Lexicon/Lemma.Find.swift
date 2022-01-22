d2a2c7b0540cd68cb176d1fda66afd09501d8834
Milos <milos@screensailor.net>
2022-01-22 20:57:29 +0000

    improve find

--- Sources/Lexicon/Lemma.Find.swift
+++ Sources/Lexicon/Lemma.Find.swift
@@ -2,27 +2,29 @@
 // github.com/screensailor 2022
 //
 
+import Collections
+
 public extension Lemma {
     
     // TODO: return an AsyncSequence
     
-    @inlinable func find(_ prefixes: String...) async -> Set<Lemma> {
-        await find(prefixes)
+    @inlinable func find(_ prefixes: String..., max: Int = 1000) async -> OrderedSet<Lemma> {
+        await find(prefixes, max: max)
     }
     
-    func find<Prefixes>(_ prefixes: Prefixes) async -> Set<Lemma> where Prefixes: Collection, Prefixes.Element == String {
+    func find<Prefixes>(_ prefixes: Prefixes, max: Int = 1000) async -> OrderedSet<Lemma> where Prefixes: Collection, Prefixes.Element == String {
         let prefixes = prefixes.drop(while: \.isEmpty)
         guard let prefix = prefixes.first else {
             return []
         }
         let rest = prefixes.dropFirst().drop(while: \.isEmpty)
-        var o: Set<Lemma> = []
-        for await lemma in breadthFirstTraversal.dropFirst() {
+        var o: OrderedSet<Lemma> = []
+        for await lemma in breadthFirstTraversal.dropFirst() where o.count < max {
             if prefix.localizedCaseInsensitiveCompare(lemma.name.prefix(prefix.count)) == .orderedSame {
                 if rest.isEmpty {
-                    o.insert(lemma)
+                    o.append(lemma)
                 } else {
-                    await o.formUnion(lemma.find(rest))
+                    await o.append(contentsOf: lemma.find(rest, max: max - o.count))
                 }
             }
         }
