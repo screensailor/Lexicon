0a7ee69bdd6d2ac8b6a95600908c6c84d408d2da
Milos <milos@screensailor.net>
2022-02-11 19:06:11 +0000

    add View.on(_: I, _: (Event) -> ()) -> some View

--- Sources/SwiftLexicon/SwiftUI.swift
+++ Sources/SwiftLexicon/SwiftUI.swift
@@ -13,3 +13,23 @@ public extension EnvironmentValues {
     var events: Events { self[EventsKey.self] }
 }
 
+public extension View {
+    
+    func on(_ event: I, _ ƒ: @escaping @MainActor (Event) -> ()) -> some View {
+        modifier(OnEvent(event: event, ƒ: ƒ))
+    }
+}
+
+public struct OnEvent: ViewModifier {
+    
+    @Environment(\.events) var events
+    
+    let event: I
+    let ƒ: @MainActor (Event) -> ()
+    
+    public func body(content: Content) -> some View {
+        content.onReceive(events.filter{ $0.is(event) }) { event in
+            ƒ(event)
+        }
+    }
+}
