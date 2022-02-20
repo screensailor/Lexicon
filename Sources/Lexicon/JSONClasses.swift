b64a119f3453195e8e5b05ec99f07023bddfda5c
Milos <milos@screensailor.net>
2022-02-20 12:54:02 +0000

    ensure consistent date formatting with JSON generator

--- Sources/Lexicon/JSONClasses.swift
+++ Sources/Lexicon/JSONClasses.swift
@@ -10,8 +10,44 @@ public enum JSONClasses: CodeGenerator {
     public static let utType: UTType = .json
     
     public static func generate(_ json: Lexicon.Graph.JSON) throws -> Data {
-        let encoder = JSONEncoder()
+        let encoder = Encoder()
         encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
         return try encoder.encode(json)
+    }
+    
+    public class Encoder: JSONEncoder {
+        public override init() {
+            super.init()
+            self.dateEncodingStrategy = .formatted(DateFormatter.shared)
+        }
+    }
+    
+    public class Decoder: JSONDecoder {
+        public override init() {
+            super.init()
+            self.dateDecodingStrategy = .formatted(DateFormatter.shared)
+        }
+    }
+
+    public class DateFormatter: Foundation.DateFormatter {
+        
+        public static let shared = DateFormatter()
+        
+        public private(set) lazy var iso: ISO8601DateFormatter = {
+            self.dateStyle = .long
+            self.timeStyle = .short
+            let o = ISO8601DateFormatter()
+            o.formatOptions.insert(.withFractionalSeconds)
+            o.timeZone = .none
+            return o
+        }()
+        
+        public override func date(from string: String) -> Date? {
+            iso.date(from: string)
+        }
+        
+        public override func string(from date: Date) -> String {
+            iso.string(from: date)
+        }
     }
 }
